84fce0a86bbb89a377be82a415a1e6323e75d661
mandel84 <mandeltech1@gmail.com>
2024-03-03 23:27:57 +0200

    exercises-day1

--- week2/day1/basic-types.py
+++
@@ -1,3 +0,0 @@
-#getting satarte
-
-print('Hello Python!!')
---
+++ week2/day1/ex-day.py
@@ -0,0 +1,93 @@
+#day1
+#ex1
+#print('Hello World\n' *4)
+
+#ex2
+#print((99**3)*8 )
+
+#ex3
+#print('1st', 5 < 3) #false
+#print('2st', 3 == 3) #true
+#print('3st', 5 < "3") #error
+#print('4st', "3" > 3) #error
+#print('5st', "Hello" == "hello ") #true
+
+#ex4
+#brand = "macintosh"
+#print("I have a " +  brand +  " computer.")
+
+#ex5
+# my_name = 'Jorge'
+# my_age = '39'
+# shoe_size = '41'
+# info = 'Hi, my name is '+ my_name + ', i have '+ my_age +' years old, and my shoe size is ' +shoe_size + '.'
+# print(info)  
+
+#ex6
+# var_a = 10
+# var_b = 7
+# if var_a > var_b:print('Hello World')
+
+#ex7
+# Prompting the user to input a number
+#number = int(input("Enter a number: "))
+
+# Checking if the number is odd or even
+#if number % 2 == 0:
+   # print(str(number) + " is even.")
+#else:
+   # print(str(number) + " is odd.")
+   
+   #ex8
+# my_name ="Asdrubal"
+# user_name = input('Enter your name: ')
+# if user_name == my_name:
+#     print(" I'm not the only one, :) ")
+# if user_name != my_name:
+#     print('thank you ')
+
+#ex9
+# height = int(input("Please, enter your height here: "))
+# if height >= 145:
+#     print("You are tall enough to ride.")
+# else:
+#     print("You need to grow some more to ride.")
+
+
+
+
+#dailychallenge
+#1
+# sentence = input("Write a sentence: ")
+# if len(sentence) < 10:
+#     print("String not long enough.")
+# elif len(sentence) > 10:
+#     print("String too long.")
+# else:
+#     print("Perfect string.")
+
+# #2
+# if sentence:
+#     print("First character:", sentence[0])
+#     print("Last character:", sentence[-1])
+
+# #3
+# for i in range(1, len(sentence) + 1):
+#     print(sentence[:i])
+    
+# #4
+# import random
+# characters = list(sentence)
+# random.shuffle(characters)
+# jumbled_text = ''.join(characters)
+# print(jumbled_text)
+
+
+
+
+   
+   
+
+
+
+
